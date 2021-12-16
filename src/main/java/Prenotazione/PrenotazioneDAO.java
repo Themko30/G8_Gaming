0780f9b6a405d7f92c902e26abd787caab3a0c94
Alfonso D'Albenzio <alfonsodalbenzio5@gmail.com>
2021-12-16 12:13:23 +0100

    Creato Prenotazione con relative componenti

---
+++ src/main/java/Prenotazione/PrenotazioneDAO.java
@@ -0,0 +1,119 @@
+package main.java.Prenotazione;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import main.java.Catalogo.Prodotto;
+import main.java.Storage.ConPool;
+
+public class PrenotazioneDAO {
+
+  public boolean doSavePrenotazione(Prenotazione prenotazione){
+    try (Connection con = ConPool.getConnection()) {
+
+      PreparedStatement ps = con.prepareStatement("INSERT INTO Prenotazione(categoria, descrizione, emailRichiedente, copertina) VALUES(?,?,?,?,?)");
+
+      ps.setString(1, prenotazione.getCategoria());
+      ps.setString(2, prenotazione.getDescrizione());
+      ps.setString(3, prenotazione.getEmailRichiedente());
+      ps.setString(4, prenotazione.getCopertina());
+
+      int x= ps.executeUpdate();
+
+      return x == 1;
+
+    }
+    catch (SQLException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public ArrayList<Prenotazione> doRetrievePrenotazione(int limit, int offset){
+    try (Connection con = ConPool.getConnection()) {
+
+      PreparedStatement ps = con.prepareStatement("SELECT * FROM Prenotazione LIMIT ?,?");
+      ps.setInt(1, limit);
+      ps.setInt(2, offset);
+
+      ResultSet rs= ps.executeQuery();
+      ArrayList<Prenotazione> prenotazioni= new ArrayList<>();
+
+      while (rs.next()){
+        Prenotazione p = new Prenotazione();
+        p.setNumeroPrenotazione(rs.getInt("numeroPrenotazione"));
+        p.setCategoria(rs.getString("categoria"));
+        p.setDescrizione(rs.getString("descrizione"));
+        p.setEmailRichiedente(rs.getString("emailRichiedente"));
+        p.setCopertina(rs.getString("copertina"));
+        prenotazioni.add(p);
+      }
+      return prenotazioni;
+
+
+    } catch(SQLException e){
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  public Prenotazione doRetrievePrenotazioneByCodice(int codice){
+    try (Connection con = ConPool.getConnection()) {
+
+      PreparedStatement ps = con.prepareStatement("SELECT * FROM Prenotazione WHERE codice=?");
+      ps.setInt(1,codice);
+
+      ResultSet rs= ps.executeQuery();
+      Prenotazione p= new Prenotazione();
+
+      if(rs.next()){
+        p.setNumeroPrenotazione(rs.getInt("numeroPrenotazione"));
+        p.setCategoria(rs.getString("categoria"));
+        p.setDescrizione(rs.getString("descrizione"));
+        p.setEmailRichiedente(rs.getString("emailRichiedente"));
+        p.setCopertina(rs.getString("copertina"));
+
+      }
+      return p;
+
+    } catch(SQLException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public boolean doUpdatePrenotazione(Prenotazione prenotazione){
+
+    try (Connection con = ConPool.getConnection()) {
+      PreparedStatement ps = con.prepareStatement("UPDATE Prenotazione SET numeroPrenotazione=?, categoria=?, descrizione=?, emailRichiedente=?, copertina=? WHERE codice=?");
+
+      ps.setInt(1, prenotazione.getNumeroPrenotazione());
+      ps.setString(2, prenotazione.getCategoria());
+      ps.setString(3, prenotazione.getDescrizione());
+      ps.setString(4, prenotazione.getEmailRichiedente());
+      ps.setString(5, prenotazione.getCopertina());
+
+      int x=ps.executeUpdate();
+
+      return x == 1;
+
+    } catch (SQLException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public boolean doDeletePrenotazione(int codice) {
+    try (Connection con = ConPool.getConnection()) {
+
+      PreparedStatement ps = con.prepareStatement("DELETE FROM Prenotazione WHERE codice=?");
+      ps.setInt(1, codice);
+
+      int x = ps.executeUpdate();
+      return x == 1;
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
---
+++ src/main/java/Prenotazione/PrenotazioneServlet.java
@@ -0,0 +1,74 @@
+package main.java.Prenotazione;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "PrenotazioneServlet", value = "/prenotazione/*")
+@MultipartConfig
+
+public class PrenotazioneServlet extends HttpServlet {
+
+  private PrenotazioneDAO prenotazioneDAO;
+
+  @Override
+  public void init() throws ServletException{
+    super.init();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doGet(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doPost(req, resp);
+
+    String path = req.getPathInfo();
+
+    switch (path){
+      case "/":
+        RequestDispatcher dispatcher = req.getRequestDispatcher("DISPLAY PAGE");
+        dispatcher.forward(req, resp);
+        break;
+      case "/save":
+        Prenotazione savePrenotazione = new Prenotazione();
+        savePrenotazione.setEmailRichiedente(req.getParameter("email"));
+        savePrenotazione.setCategoria(req.getParameter("categoria"));
+        /*savePrenotazione.setCopertina();*/ // DA FARE PER CONFING MULTIPART
+        savePrenotazione.setDescrizione(req.getParameter("descrizione"));
+        if (prenotazioneDAO.doSavePrenotazione(savePrenotazione)) {
+          resp.setStatus(HttpServletResponse.SC_CREATED);
+          req.getRequestDispatcher("VIEW PAGE DA FARE").forward(req,resp);
+        } else {
+          //internalError DA FARE
+        }
+        break;
+        case "/update":
+          Prenotazione updatePrenotazione = new Prenotazione();
+          updatePrenotazione.setEmailRichiedente(req.getParameter("email"));
+          updatePrenotazione.setCategoria(req.getParameter("categoria"));
+          /*updatePrenotazione.setCopertina();*/ // DA FARE PER CONFING MULTIPART
+          updatePrenotazione.setDescrizione(req.getParameter("descrizione"));
+          if (prenotazioneDAO.doUpdatePrenotazione(updatePrenotazione)) {
+            // SET ALERT
+            req.getRequestDispatcher("VIEW PAGE DA FARE").forward(req,resp);
+          } else {
+            //internalError DA FARE
+          }
+          break;
+          case "/delete":
+            if (prenotazioneDAO.doDeletePrenotazione(Integer.valueOf(req.getParameter("codice"))));
+            // SET ALERT
+            req.getRequestDispatcher("VIEW PAGE DA FARE").forward(req,resp);
+    }
+
+  }
+}
