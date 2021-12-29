ed85d6588005a9f4f52819f82a743525cf281fc3
Salvatore <s.danese@studenti.unisa.it>
2021-12-29 15:50:49 +0100

    CarrelloServlet unificata

--- src/main/java/Carrello/CarrelloService.java
+++ src/main/java/Carrello/CarrelloService.java
@@ -1,5 +1,7 @@
 package main.java.Carrello;
 
+import main.java.Autenticazione.Utente;
+
 public interface CarrelloService {
 
     Carrello aggiungiProdotto(Carrello carrello, int codiceProdotto, int quantita);
@@ -11,4 +13,6 @@ public interface CarrelloService {
     public Carrello clearCarrello(Carrello carrello);
 
     public boolean updateCarrello(Carrello carrello);
+
+    void creaCarrello(Utente utente);
 }
