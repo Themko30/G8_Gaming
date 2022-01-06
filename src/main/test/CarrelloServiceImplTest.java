5571e0fe18621285e24b2ac695551b0b89fd4037
Salvatore <s.danese@studenti.unisa.it>
2022-01-06 17:49:03 +0100

    CarrelloServiceImplTest

--- src/main/test/CarrelloServiceImplTest.java
+++ src/main/test/CarrelloServiceImplTest.java
@@ -29,6 +29,7 @@ public class CarrelloServiceImplTest {
         when(prodotto.getCodice()).thenReturn(1);
         when(prodotto.getPrezzo()).thenReturn(100.0);
         when(prodotto.getScontoAttivo()).thenReturn(0.25);
+        when(prodotto.getQuantita()).thenReturn(50);
         Whitebox.setInternalState(carrello, "numeroArticoli", 1);
         Whitebox.setInternalState(carrello, "totale", 75.0);
         Whitebox.setInternalState(carrello, "prodotti", prodotti);
@@ -45,6 +46,34 @@ public class CarrelloServiceImplTest {
 
         assertTrue(carrelloVero.getTotale()==150.0);
         assertTrue(carrelloVero.getProdotti().get(prodotto)==2);
+
+    }
+
+    @Test
+    public void testUpdateQuantitaCarrelloSessionOverLimit(){
+        CarrelloServiceImpl carrelloService = new CarrelloServiceImpl();
+        LinkedHashMap<Prodotto, Integer> prodotti = new LinkedHashMap<>();
+        prodotti.put(prodotto, 1);
+        when(prodotto.getCodice()).thenReturn(1);
+        when(prodotto.getPrezzo()).thenReturn(100.0);
+        when(prodotto.getScontoAttivo()).thenReturn(0.25);
+        when(prodotto.getQuantita()).thenReturn(1);
+        Whitebox.setInternalState(carrello, "numeroArticoli", 1);
+        Whitebox.setInternalState(carrello, "totale", 75.0);
+        Whitebox.setInternalState(carrello, "prodotti", prodotti);
+        when(carrello.getNumeroArticoli()).thenCallRealMethod();
+        when(carrello.getTotale()).thenCallRealMethod();
+        when(carrello.getProdotti()).thenCallRealMethod();
+        doCallRealMethod().when(carrello).setNumeroArticoli(any(Integer.class));
+        doCallRealMethod().when(carrello).setTotale(any(Double.class));
+        doCallRealMethod().when(carrello).setProdotti(any(LinkedHashMap.class));
+
+
+        Carrello carrelloVero = carrelloService.updateQuantitaCarrelloSession(carrello, 1, 2);
+        assertTrue(carrelloVero.getNumeroArticoli()==1);
+
+        assertTrue(carrelloVero.getTotale()==75.0);
+        assertTrue(carrelloVero.getProdotti().get(prodotto)==1);
 
     }
 
